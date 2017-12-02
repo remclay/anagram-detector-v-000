52764cb4d56d5d2663fffd437474e3fb3915bdd0
Remmy Clay <remmy.lauren@gmail.com>
2017-12-02 00:45:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,7 @@ attr_accessor :word
   end
 
   def match(anagrams)
-    anagrams.find_all do |w|
-      w.split("").sort == @word.split("").sort
-    end
+    anagrams.find_all {|w| w.split("").sort == @word.split("").sort}
     # split anagrams array, arrange each (.sort), then compare with @word using enumerator that returns an array
   end
 
