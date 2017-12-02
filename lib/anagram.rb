d0d14360fe245ad98960c29adf3a1f7a554a53af
Remmy Clay <remmy.lauren@gmail.com>
2017-12-02 01:05:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.find_all {|anagram| anagram.split("").sort == @word.split("").sort}
+  end
+
+end
