e6d123dc30abad1e2fc3d8a38d7cafaee4e1f2ce
Remmy Clay <remmy.lauren@gmail.com>
2017-12-02 00:31:51 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.split(', ')
+    # split anagrams array, arrange each (.sort), then compare with @word using enumerator that returns an array
+  end
+
+end
